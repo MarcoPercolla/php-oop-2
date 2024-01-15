25ee9db9c5a1a05c9f13e05294886436664894e8
ReDMallard <marco@titori.it>
2024-01-15 16:25:19 +0100

    test tramite vardump ok

--- data.php
+++ data.php
@@ -1,3 +1,20 @@
 <?php
 require_once __DIR__ . '/Models/Gioco.php';
 require_once __DIR__ . '/Models/Cibo.php';
+
+$prodotti = [
+    new Gioco(
+        "palla",
+        "4$", 
+        "https://labottegadelpet.it/wp-content/uploads/2020/11/83489.jpg",
+        new Categoria("cane","tutte","tutte"),
+        "silicone",
+        "masticazione"
+    ),
+    new Prodotto(
+        "osso",
+        "6$", 
+        "https://labottegadelpet.it/wp-content/uploads/2020/11/83489.jpg",
+        new Categoria("cane","m","adulto"),
+    ),
+];
--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php
 require_once __DIR__ . '/Data.php';
+var_dump($prodotti);
